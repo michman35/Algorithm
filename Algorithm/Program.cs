3f5d74ab29438787a8764fab54a1dbe8d1e7df45
User <User@DESKTOP-RLOLFP4>
2021-09-28 20:05:13 +0300

    Lesson1

---
+++ Algorithm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Algorithm
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("введите число");
+			int n = Convert.ToInt32(Console.ReadLine());
+			Simple number = new Simple();
+			number.simpes(n);
+		}
+	}
+}
