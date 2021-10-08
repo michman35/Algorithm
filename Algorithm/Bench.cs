acb04b6ee16865f7b85acc58e90b155e85639aaf
User <User@DESKTOP-RLOLFP4>
2021-10-08 13:33:04 +0300

    lesson3

---
+++ Algorithm/Bench.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using static Algorithm.PointClass;
+
+namespace Algorithm
+{
+	class Bench
+	{
+        [Benchmark]
+        public void Test()
+        {
+            PointClas point = new PointClas();
+            point.PointDistance1();
+        }
+        [Benchmark]
+        public void TestTwo()
+        {
+            PointStructF point = new PointStructF();
+            point.PointDistance2();
+        }
+        [Benchmark]
+        public void TestThree()
+        {
+            PointStructD point = new PointStructD();
+            point.PointDistance3();
+        }
+        [Benchmark]
+        public void TestFour()
+        {
+            PointStructF point = new PointStructF();
+            point.PointDistance4();
+        }
+    }
+}
+}
---
+++ Algorithm/PointClass.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+
+namespace Algorithm
+{
+    class PointClass
+    {
+
+
+        public readonly int razmer = 100; // размер массива данных
+
+        public static PointStructF[] masF = new PointStructF[razmer + 1];
+        public static PointStructD[] masD = new PointStructD[razmer + 1];
+        public static PointClass[] masC = new PointClass[razmer + 1];
+        public class PointClas // вариант для ссылочного типа
+        {
+            public float X;
+            public float Y;
+            public class PointClas // вариант для ссылочного типа
+            public float PointDistance1(PointClas pointOne, PointClas pointTwo) // вариант теста 1
+            {
+                float x = pointOne.X - pointTwo.X;
+                float y = pointOne.Y - pointTwo.Y;
+                return MathF.Sqrt((x * x) + (y * y));
+            }
+        }
+        public struct PointStructF // вариант для типа float
+        {
+            public float X;
+            public float Y;
+            public float PointDistance2(PointStructF pointOne, PointStructF pointTwo) // вариант теста 2
+            {
+                float x = pointOne.X - pointTwo.X;
+                float y = pointOne.Y - pointTwo.Y;
+                return MathF.Sqrt((x * x) + (y * y));
+            }
+
+        }
+        public struct PointStructD // вариант для типа double
+        {
+            public double X;
+            public double Y;
+            public double PointDistance3(PointStructD pointOne, PointStructD pointTwo) // вариант теста 3
+            {
+                double x = pointOne.X - pointTwo.X;
+                double y = pointOne.Y - pointTwo.Y;
+                return Math.Sqrt((x * x) + (y * y));
+            }
+        }
+        public float PointDistance4(PointStructF pointOne, PointStructF pointTwo) // вариант теста 4
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return Sqrt((x * x) + (y * y));
+        }
+        public float Sqrt(float x)
+        {
+            float s = ((x / 2) + x / (x / 2));
+            for(int i = 0; i < 4; i++)
+            {
+                s = (s + x / s) / 2;
+            }
+            return s;
+
+
+
+        }
+    } 
+}
+
+
