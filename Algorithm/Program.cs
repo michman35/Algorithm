f7bb3c02f0c699fb0914b5d3f322fd80c96dc292
User <User@DESKTOP-RLOLFP4>
2021-10-25 18:05:11 +0300

    lesson7

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -2,38 +2,19 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.NetworkInformation;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
 
 namespace Algorithm
 {
-    class Program
+class Program
     {
-
         static void Main(string[] args)
         {
-            Node nodee = new Node();
-
-            nodee.Tree();
-            nodee.PreOrderTravers(nodee);
-            Console.WriteLine();
-
-            Console.Write("Stack: ");
-            nodee.Stack(nodee);
-            Console.WriteLine();
-            Console.Write("Queue: ");
-            nodee.Queue(nodee);
+            Square way = new Square();
+            way.SquareWalk();
         }
     }
 }
-    /* Сергей , понял что мой код к предыдщему задания , мягко сказать не очень , поэтому поьзуясь 
-    вашим материалом переписал дерево с поиском в глубину и ширину используя очередь и стек . С графом засел 
-     в тупик , данные обходы не подойдут для него как я понял из методички . И что бы описать граф , мне 
-     нужно сначала описать его вершину и ребра . А затем написать класс описывающий граф с методами добавления вершин и ребер.
-     Все правильно ? */
-
-
-
-
-	
